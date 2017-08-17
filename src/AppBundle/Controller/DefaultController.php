c51f1d4cfcdd5d9dffe5945a054bd75d5348a883
Brian Feaver <brian.feaver@gmail.com>
2017-08-17 10:48:09 -0700

    Add quick API route to return our Reddit article response

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Request\RedditArticleRequest;
+use AppBundle\Service\RedditService;
+use JMS\Serializer\Serializer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -17,5 +21,22 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/api", name="api")
+     */
+    public function apiAction(Request $request, RedditService $redditService, Serializer $serializer)
+    {
+        // Add validation if time permits
+        $redditRequest = new RedditArticleRequest();
+        $redditRequest->setType($request->query->get('type'));
+        $redditRequest->setSubreddit($request->query->get('subreddit'));
+        $redditRequest->setCount($request->query->get('count'));
+        $redditRequest->setAfter($request->query->get('after'));
+
+        $response = $redditService->getArticles($redditRequest);
+
+        return new JsonResponse($serializer->serialize($response, 'json'), 200, [], true);
     }
 }
